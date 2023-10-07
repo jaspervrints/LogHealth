39695c5c719476b7c1c19b0c306457411c727e39
Jasper Vrints <jasper.vrints@gmail.com>
2023-10-07 14:36:54 +0200

    initial maui

---
+++ LogHealth/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace LogHealth;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ LogHealth/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace LogHealth;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
